11bbc64a6053d5ebd08ef9053b71a0295d319b09
Christopher Cahill <cthomascahill@gmail.com>
2018-12-07 09:31:07 -0800

    return all methods

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,24 @@ attr_accessor :title, :id
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    return_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    return_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
+  end
+    projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,29 @@ attr_accessor :name, :project_id, :id
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      id = volunteer.fetch("id").to_i()
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+  end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
+  end
+
+
+
+
+
 end
