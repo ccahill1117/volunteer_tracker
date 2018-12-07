e3735eff06a560364e6a4e28864dbafb81a39166
Christopher Cahill <cthomascahill@gmail.com>
2018-12-07 12:30:22 -0800

    visit unique project page for each project created

--- app.rb
+++ app.rb
@@ -6,3 +6,25 @@ require("./lib/volunteer")
 require("pg")
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get ("/") do
+  @volunteers = Volunteer.all()
+  @projects = Project.all()
+  erb(:index)
+
+end
+
+post ("/projects") do
+  @volunteers = Volunteer.all()
+  @projects = Project.all()
+  title = params['title']
+  description = params['description']
+  project = Project.new({:title => title, :description => description, :id => nil})
+  project.save()
+  redirect('/')
+end
+
+get("/projects/:id") do
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:projects)
+end
